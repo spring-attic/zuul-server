98391187a435d36109a4fe0c621eb0b09c91d14c
Ryan Baxter <rbaxter@pivotal.io>
2016-10-27 15:16:17 -0400

    Added demonstration of hystrix fallback

--- src/main/java/zuulserver/ZuulServerApplication.java
+++ src/main/java/zuulserver/ZuulServerApplication.java
@@ -1,24 +1,84 @@
 package zuulserver;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Spencer Gibb
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@Controller
+@SpringBootApplication
 @EnableZuulProxy
+@RestController
 public class ZuulServerApplication {
 	
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZuulServerApplication.class).web(true).run(args);
+    }
+
+    @RequestMapping("/timeout")
+    public String timeout() throws InterruptedException {
+        Thread.sleep(80000);
+        return "timeout";
+    }
+
+    @Bean
+    public ZuulFallbackProvider zuulFallbackProvider() {
+        return new ZuulFallbackProvider() {
+            @Override
+            public String getRoute() {
+                return "zuulserver";
+            }
+
+            @Override
+            public ClientHttpResponse fallbackResponse() {
+                return new ClientHttpResponse() {
+                    @Override
+                    public HttpStatus getStatusCode() throws IOException {
+                        return HttpStatus.OK;
+                    }
+
+                    @Override
+                    public int getRawStatusCode() throws IOException {
+                        return 200;
+                    }
+
+                    @Override
+                    public String getStatusText() throws IOException {
+                        return "OK";
+                    }
+
+                    @Override
+                    public void close() {
+
+                    }
+
+                    @Override
+                    public InputStream getBody() throws IOException {
+                        return new ByteArrayInputStream("fallback".getBytes());
+                    }
+
+                    @Override
+                    public HttpHeaders getHeaders() {
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.setContentType(MediaType.APPLICATION_JSON);
+                        return headers;
+                    }
+                };
+            }
+        };
     }
 
 }
